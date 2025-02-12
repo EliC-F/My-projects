b47cc2803280d699ef63676709268d2779cab3c9
Eliã Carlos Frazão Sá <eliac@acad.ifma.edu.br>
2025-02-12 12:19:10 -0300

    Add files via upload
    Nesse código eu desenvolvo um programa que mostre o sucessor e o antecessor de um numero

---
+++ ex005(ante. e sucess.).py
@@ -0,0 +1,3 @@
+n = int(input('Digite um numero: '))
+print(f'O numero que você digitou é o \033[32m{n}\033[m, o seu antecessor é o '
+      f'\033[33m{n - 1}\033[m e o seu sucessor é o \033[34m{n + 1}\033[m')
