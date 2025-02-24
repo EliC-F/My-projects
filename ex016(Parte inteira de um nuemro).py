04fd776a183d7206552fa4b606b63cf965b7bed6
Eliã Carlos Frazão Sá <eliac@acad.ifma.edu.br>
2025-02-24 13:08:29 -0300

    Add files via upload

---
+++ ex016(Parte inteira de um nuemro).py
@@ -0,0 +1,7 @@
+from math import floor
+#from math import trunc
+n = float(input('Digite um numero: '))
+i = floor(n)
+# i1 = trunc(n) Outra forma de fazer esse exercicio
+print(f'A parte inteira de \033[31m{n}\033[m é \033[36m{i}\033[m')
+#Print(f'A parte inteira de {n} é {int(n)}')
